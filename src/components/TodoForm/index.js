b5067fc4eda37157adfefaaf9627eac4caa9514d
Rami Elsayed <ramielsayed123@outlook.com>
2022-09-24 15:32:34 +0100

    date picker added

--- src/components/TodoForm/index.js
+++ src/components/TodoForm/index.js
@@ -1,3 +1,36 @@
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
 export const TodoForm = ({ submit }) => {
-  return <div>todo form </div>;
+  return (
+    <section className="container">
+      <form>
+        <div class="mb-3">
+          <input
+            type="text"
+            class="form-control"
+            id="title"
+            aria-describedby="Title"
+          />
+        </div>
+        <div class="mb-3">
+          <textarea
+            class="form-control"
+            placeholder="Add some details"
+            id="content"
+          ></textarea>
+        </div>
+        <div class="mb-3">
+          <label for="dueDate" class="form-label">
+            Due Date
+          </label>
+          <DatePicker id="dueDate" selected={Date.now()} />
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </section>
+  );
 };
